510006885c4198f54c739b73da3ce51057931b48
Nikola Lazarov <nikola.n.lazarov@progress.com>
2023-10-01 13:00:55 +0300

    wip

---
+++ src/app/blog/[slug]/page.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+
+const BlogDetails = () => (
+  <div className="prose mx-auto pb-12">
+    <h1>Article Title</h1>
+    <Image alt="" src="https://placehold.co/1496x720.svg" width="584" height="282" />
+    <p>
+      Maecenas ante velit, venenatis non fringilla cursus, suscipit a lectus. Pellentesque iaculis, eros eleifend aliquam eleifend, turpis est molestie urna, non tempor sapien turpis quis justo. Nullam convallis neque sed odio varius, ac volutpat velit scelerisque. Fusce condimentum arcu sed pretium commodo. Curabitur quis ipsum maximus, vehicula ante ut, tincidunt tortor. Pellentesque efficitur quam id neque tempus, nec volutpat nibh interdum. Sed sagittis quis ligula in fermentum. Donec congue ipsum non arcu accumsan, sit amet condimentum tortor eleifend. Vivamus diam dui, gravida in nibh sit amet, gravida iaculis sem. Curabitur ut nisl ornare velit sagittis semper.
+    </p>
+    <p>
+      Cras molestie maximus ultrices. Sed sollicitudin bibendum tellus, in consectetur augue bibendum sed. Ut molestie mi ac posuere faucibus. Nulla vitae urna sit amet erat viverra finibus. Curabitur bibendum sagittis libero, non ultrices mauris. In hac habitasse platea dictumst. Suspendisse vestibulum vulputate justo, id porttitor ante faucibus sed. Fusce ultricies egestas egestas. Duis suscipit nunc id bibendum consequat. Suspendisse blandit molestie ex, non porta enim fringilla a. Suspendisse elementum accumsan orci, vitae finibus sem suscipit in.
+    </p>
+    <p>
+      Fusce molestie quis ipsum a suscipit. Vivamus felis dui, finibus at libero at, convallis cursus metus. Cras a nisi at nisi placerat pulvinar vitae et ante. Donec auctor dui a enim maximus, quis pharetra magna tristique. Phasellus ultricies sollicitudin commodo. Vestibulum et eros in lorem sagittis finibus at eu nibh. Duis id molestie felis. Sed vulputate mauris tortor, ac facilisis tortor porttitor nec.
+    </p>
+    <p>
+      Nunc viverra, neque nec luctus lacinia, purus libero consectetur nulla, ac semper turpis turpis non eros. Donec vitae vehicula metus, quis placerat felis. In hac habitasse platea dictumst. Proin efficitur felis id leo consequat sollicitudin. Sed nec tristique risus. Mauris scelerisque lorem eros, id aliquam sem ullamcorper sed. Duis vitae aliquam elit, mattis semper dui. Aliquam efficitur nulla et arcu congue bibendum. Donec quam mi, feugiat in consequat vitae, sodales sed justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices molestie consequat. Vivamus vitae mattis sem. Nulla ut ornare lectus, id faucibus sem.
+    </p>
+    <p>
+      Donec dignissim vel nibh id scelerisque. In luctus sapien risus, et ornare lorem tempus a. Maecenas rutrum pellentesque nulla id tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus sed mi vitae nulla porta iaculis vel quis augue. Maecenas velit nibh, accumsan quis ultrices ut, semper sit amet erat. Fusce bibendum odio lectus, ut ullamcorper urna imperdiet sed. Praesent vitae dui eget diam placerat tempus. Donec urna lorem, laoreet tincidunt augue vel, ultrices elementum neque. Pellentesque massa enim, luctus nec iaculis quis, aliquam tincidunt nisi. Quisque accumsan diam dolor, vitae molestie quam ornare quis. Ut et justo ut neque finibus rutrum. Integer dictum, dolor a pretium dictum, nisi velit bibendum risus, id euismod magna dolor a odio. Mauris mauris velit, venenatis nec posuere non, porttitor non libero.
+    </p>
+  </div>
+);
+
+export default BlogDetails;
