dfcc8338c0d54e9fed2b905a9d6e810a2d6f7257
Nikola Lazarov <nikola.n.lazarov@progress.com>
2023-10-08 13:48:55 +0300

    WiP mobile navigation

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -12,7 +12,7 @@ const Header = () => (
       <Link href="/" className="block mb-7 hover:opacity-80 transition">
         <Image src={logo} alt="naydenova.studio logo" width={240} height={111} />
       </Link>
-      <nav className={`flex gap-x-10 ${comfortaa.className} font-light text-lg`}>
+      <nav className={`nav flex gap-x-10 ${comfortaa.className} font-light text-lg`}>
         <NavLink href="/gallery" className='navLink'>Gallery</NavLink>
         <NavLink href="/about" className='navLink'>About</NavLink>
         <NavLink href="/blog" className='navLink'>Blog</NavLink>
