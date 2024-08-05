77c3834ae55cd6f991a5a4aff0f82b617e0f6d8d
Nikola Lazarov <nikola.n.lazarov@progress.com>
2024-08-05 17:08:31 +0300

    update contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,13 +1,14 @@
 import ContactForm from '../components/contact-form';
 import Container from '../components/container';
 import Image from 'next/image';
+import contactImage from '../../assets/dora-naydenova-contact-image.webp';
 
 const Contact = () => (
   <Container className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-12 gap-8 pt-12 pb-24 px-5">
     <div className="xl:col-start-3 xl:col-end-6">
-      <Image src="https://placehold.co/350x300.svg" width={350} height={300} className="mb-5" />
-      <p className="mb-2">Contact me when you fill the contact form</p>
-      <p>Or email me at <a href="mailto:dora@naydenova.studio">dora@naydenova.studio</a></p>
+      <Image src={contactImage} width={350} height={300} className="mb-5" />
+      <p className="mb-2">Имаш идея за картина?</p>
+      <p>Остави ми запитване.</p>
     </div>
     <div className="xl:col-start-7 xl:col-end-11">
       <ContactForm />
